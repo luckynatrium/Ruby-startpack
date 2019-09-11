d33d2c02cf3f5d81104d2531ed71cf6532630898
luckynatrium <boris.wertik@gmail.com>
2019-09-11 16:06:55 +0300

    Back to normal

--- koans/about_symbols.rb
+++ koans/about_symbols.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutSymbols < Neo::Koan
   def test_symbols_are_symbols
-    symbol = :r
+    symbol = :ruby
     assert_equal true, symbol.is_a?(Symbol)
   end
 
